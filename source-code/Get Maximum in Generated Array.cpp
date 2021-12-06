a96a2f8a295804b922a176a6d522659e2911db59
Anik Mahmud <55916366+anik107@users.noreply.github.com>
2021-12-06 15:44:08 +0600

    cpp

---
+++ source-code/Get Maximum in Generated Array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int getMaximumGenerated(int n) {
+        int dp[n+2];
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            if(i%2==0)dp[i]=dp[i/2];
+            else dp[i]=dp[i/2]+dp[i/2+1];
+        }
+        return *max_element(dp,dp+1+n);
+    }
+};
