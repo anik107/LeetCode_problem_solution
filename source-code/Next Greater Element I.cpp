a07d64ab5f9e295ad74f27f6a18fd5815d745e2e
Anik Mahmud <55916366+anik107@users.noreply.github.com>
2021-12-11 23:20:31 +0600

    monotonic stack

---
+++ source-code/Next Greater Element I.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        stack<int>s;
+        unordered_map<int,int>mp;
+        for(auto x: nums2){
+            while(!s.empty() && s.top()<x){
+                mp[s.top()]=x;
+                s.pop();
+            }
+            s.push(x);
+        }
+        vector<int>ans;
+        for(auto x: nums1){
+            if(mp.count(x)>0)ans.push_back(mp[x]);
+            else ans.push_back(-1);
+        }
+        return ans;
+    }
+};
