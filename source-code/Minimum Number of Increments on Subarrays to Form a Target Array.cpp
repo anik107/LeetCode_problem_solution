8e8b887118caa8787c5563768976c88b553bb23d
Anik Mahmud <55916366+anik107@users.noreply.github.com>
2021-12-11 22:32:36 +0600

    monotonic stack

---
+++ source-code/Minimum Number of Increments on Subarrays to Form a Target Array.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int minNumberOperations(vector<int>& target) {
+        int ans=target[0];
+        for(int i=0;i+1<target.size();i++){
+            ans+=max(target[i+1]-target[i],0);
+        }
+        return ans;
+    }
+};
