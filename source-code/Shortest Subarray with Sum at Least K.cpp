2985e037859c1a8baa3f3f79610621a9ee526ddb
Anik Mahmud <55916366+anik107@users.noreply.github.com>
2021-12-18 16:09:44 +0600

    cpp

---
+++ source-code/Shortest Subarray with Sum at Least K.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int shortestSubarray(vector<int>& nums, int k) {
+        int n=nums.size();
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        long long sum=0;
+        int ans=INT_MAX;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+            if(sum>=k)ans=min(ans,i+1);
+            while(!pq.empty() && sum-pq.top().first>=k){
+                ans=min(ans,i-pq.top().second);
+                pq.pop();
+            }
+            pq.push({sum,i});
+        }
+        return (ans==INT_MAX)?-1:ans;
+    }
+};
