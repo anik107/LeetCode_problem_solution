f1f3fa585be9860ff70388679db07cdaa0c379af
Anik Mahmud <55916366+anik107@users.noreply.github.com>
2022-01-13 11:37:22 +0600

    cpp

---
+++ source-code/Single Number II.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        map<int,int>mp;
+        for(auto x: nums)mp[x]++;
+        for(auto x: mp)if(x.second==1)return x.first;
+        return 0;
+    }
+};
