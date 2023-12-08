ccb1a623b98f46180cf749bf16ad31a3a25d6c18
anik107 <purplecs107@yahoo.com>
2023-12-08 11:45:26 +0600

    8-12-2023

---
+++ source-code/longest-substring-without-repeating-character.py
@@ -0,0 +1,14 @@
+s = input()
+i=0
+j=0
+n=len(s)
+disc = {}
+
+while i<n and j<n:
+    if disc.get(s[i],0)==0:
+        print(i-j+1)
+        disc[s[i]]=1
+        i+=1
+    else:
+        disc[s[j]]-=1
+        j+=1
