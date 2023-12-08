9347bf5346988342f2b910551c56d1e889562bb6
anik107 <purplecs107@yahoo.com>
2023-12-08 11:50:52 +0600

    array

--- source-code/longest-substring-without-repeating-character.py
+++ source-code/longest-substring-without-repeating-character.py
@@ -1,14 +1,16 @@
 s = input()
 i=0
 j=0
+res=0
 n=len(s)
 disc = {}
 
 while i<n and j<n:
     if disc.get(s[i],0)==0:
-        print(i-j+1)
+        res=max(res,i-j+1)
         disc[s[i]]=1
         i+=1
     else:
         disc[s[j]]-=1
         j+=1
+print(res)
